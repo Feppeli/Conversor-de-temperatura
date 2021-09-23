65be0e7fe26209a8a585d54fe03ffc99367de720
Felipe Teixeira <80359142+Feppeli@users.noreply.github.com>
2021-09-23 14:18:38 -0300

    Update conversor.js

--- conversor.js
+++ conversor.js
@@ -1,25 +1,25 @@
-document.getElementById('convert').onclick = tempConvert;
-document.getElementById('clear').onclick = clearForm;
+document.getElementById('convert').onclick = tempConvert; // retirando as informações de 'convert' e armazenando em tempCovert
+document.getElementById('clear').onclick = clearForm; // retirando as informações de 'clear' e armazenando em clearForm
 
-function tempConvert() {
-    var fahrenheit = document.getElementById("fahrenheit").value;
-    var celsius = document.getElementById("celsius").value;
+function tempConvert() { // criando a função tempConvert
+    var fahrenheit = document.getElementById("fahrenheit").value; // recebendo o valor em Fahrenheit e gaurdando em uma variável
+    var celsius = document.getElementById("celsius").value;  // recebendo o valor em Celsius e gaurdando em uma variável
 
-    if(isNaN(fahrenheit) || isNaN(celsius)){
+    if(isNaN(fahrenheit) || isNaN(celsius)){ // criando condicionais para Celsius e Fahrenheit 
         alert("digite um valor válido");
         return
-    }else if(fahrenheit === ''){
+    }else if(fahrenheit === ''){ // condição para transformar o valor recebido em um tipo Float para utilizar os calculos
         fahrenheit = (parseFloat(celsius) * 1.8) + 32
-    }else if(celsius ===''){
+    }else if(celsius ===''){    // condição para transformar o valor recebido em um tipo Float para utilizar os calculos
         celsius = (parseFloat(fahrenheit - 32)) / 1.8;
     }
 
-    document.getElementById("fahrenheit").value = parseFloat(fahrenheit).toFixed(1);
-    document.getElementById("celsius").value = parseFloat(celsius).toFixed(1);
+    document.getElementById("fahrenheit").value = parseFloat(fahrenheit).toFixed(1); // limitando o número de casas décimais que o resultado poderá ter
+    document.getElementById("celsius").value = parseFloat(celsius).toFixed(1); // limitando o número de casas décimais que o resultado poderá ter
 
 }
 
-function clearForm() {
+function clearForm() { // Função feita para limpar os campos e resetar o programa
     document.getElementById("fahrenheit").value = "";
     document.getElementById("celsius").value = "";
 }
